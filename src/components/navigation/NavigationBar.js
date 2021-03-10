af273d76d5e291524ff2cbc6fc74deecf0466f60
arttujo <arttujo@gmail.com>
2021-03-10 17:15:04 +0200

    some styling done

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -1,30 +1,30 @@
 import "../../styles/nav-styles.css";
-
 import React, { useState } from "react"
 import NavigationItem from "./NavigationItem";
-import { AppBar, IconButton, makeStyles, Menu, MenuItem, Toolbar } from "@material-ui/core";
+import { AppBar, IconButton, makeStyles, Menu, MenuItem, Toolbar, Typography } from "@material-ui/core";
 import logo from "../../assets/logo_mycampus.webp"
 import MenuIcon from "@material-ui/icons/Menu"
 import { navigate } from "hookrouter";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+
+  },
+  image: {
+    maxWidth: "150px",
+  },
+  title: {
+    flexGrow: 1
+  },
+  menuButton: {
+    color: "#FFFFFF",
+
+  },
+  offset: theme.mixins.toolbar
+}))
+
 const NavigationBar = () => {
-
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1
-    },
-    image: {
-      maxWidth: 100
-    },
-    title: {
-      flexGrow: 1
-    },
-    menuButton: {
-      marginLeft: theme.spacing(2),
-      color: "#FFFFFF"
-    }
-  }))
-
   const classes = useStyles()
   const [open, setOpen] = useState(null)
   const [width, setWidth] = useState(window.innerWidth)
@@ -45,20 +45,19 @@ const NavigationBar = () => {
   }
 
   window.addEventListener("resize", updateWidth)
- 
-
-
   return (
     <div className={classes.root}>
-      <AppBar>
+      <AppBar position="fixed">
         <Toolbar>
+
           <img src={logo} className={classes.image} />
 
-          {width < 500
-            ? <div>
-              <IconButton edge="end" className={classes.menuButton} onClick={handleMenuClick} aria-haspopup="true">
+          {width < 650 // Breakpoint for view width of 650px. Smaller width renders only harmburger menu 
+            ?<div>
+              <IconButton edge="start" className={classes.menuButton} onClick={handleMenuClick} aria-haspopup="true">
                 <MenuIcon />
               </IconButton>
+
               <Menu id="nav-menu" anchorEl={open} keepMounted open={open} onClose={handleMenuClose} >
                 <MenuItem onClick={() => handleNavigate("/")}>Home</MenuItem>
                 <MenuItem onClick={() => handleNavigate("/idea-barrel")}>Idea Barrel</MenuItem>
@@ -66,20 +65,15 @@ const NavigationBar = () => {
                 <MenuItem onClick={() => handleNavigate("/bulletin")}>Bulletin Board</MenuItem>
               </Menu>
             </div>
-            : <div>
+            :<div>
               <NavigationItem url="/" title="Home" />
               <NavigationItem url="/idea-barrel" title="Idea Barrel" />
               <NavigationItem url="/events" title="Events" />
               <NavigationItem url="/bulletin" title="Bulletin Board" />
-            </div>
-
-
-
-          }
-
-
+            </div>}
         </Toolbar>
       </AppBar>
+      <div className={classes.offset} />
     </div>
   );
 };
