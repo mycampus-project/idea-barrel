eb6f111f19d329a54f83a83c21d921b7005575e2
arttujo <arttujo@gmail.com>
2021-03-10 12:10:58 +0200

    made appbar stick on top

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -3,14 +3,19 @@ import "../../styles/nav-styles.css";
 import React from "react"
 
 import NavigationItem from "./NavigationItem";
+import { AppBar, Toolbar } from "@material-ui/core";
 
 const NavigationBar = () => {
   return (
     <div className="nav-list">
-      <NavigationItem url="/" title="Home" />
-      <NavigationItem url="/idea-barrel" title="Idea Barrel" />
-      <NavigationItem url="/events" title="Events" />
-      <NavigationItem url="/bulletin" title="Bulletin Board" />
+      <AppBar>
+        <Toolbar>
+          <NavigationItem url="/" title="Home" />
+          <NavigationItem url="/idea-barrel" title="Idea Barrel" />
+          <NavigationItem url="/events" title="Events" />
+          <NavigationItem url="/bulletin" title="Bulletin Board" />
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
