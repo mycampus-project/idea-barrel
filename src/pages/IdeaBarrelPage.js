65eb85ca87816c9125c0fd0b8eb97ac3fb05c9e4
dawcules <ilias.doukas@gmail.com>
2021-03-28 15:27:28 +0300

    remove unused imports, split ideacode to separate components

--- src/pages/IdeaBarrelPage.js
+++ src/pages/IdeaBarrelPage.js
@@ -1,19 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { DeleteForever } from "@material-ui/icons";
 import IdeaForm from "../components/ideaparts/IdeaForm";
 import IdeaList from "../components/ideaparts/IdeaList.js"
 
 import BackendAPI from "../api/BackendAPI";
 
-const { fetchIdeasAsync, fetchUsersAsync, deleteIdeaAsync } = BackendAPI();
-
-
-const dataStyle = {
-  border: "1px solid black",
-  width: "400px",
-  padding: "4px",
-  marginTop: "4px",
-};
+const { fetchIdeasAsync, fetchUsersAsync} = BackendAPI();
 
 
 const IdeaBarrelPage = () => {
@@ -53,39 +44,6 @@ const IdeaBarrelPage = () => {
         : null;
     setUser(randomUser);
   };
-
-  // An idea template to demonstrate post request to the json-server
- 
-
-  const deleteIdea = async (id, category) => {
-    try {
-      await deleteIdeaAsync(id, category);
-      console.log("deleted");
-      window.location.reload();
-    } catch (e) {
-      console.log("error deleting an idea");
-      console.log(e);
-    }
-  };
-
-
-  // The list items you see in the idea list on the "home page"
-  const ideaItem = (idea) => {
-    return (
-      <div key={idea.id} style={dataStyle}>
-        <h4>{idea.title}</h4>
-        <p>{idea.body}</p>
-        <h4>Category: {idea.category}</h4>
-        <h4>Votes: 0</h4>
-        <DeleteForever
-          onClick={() => deleteIdea(idea.id, idea.category)}
-          style={{ cursor: "pointer" }}
-        ></DeleteForever>
-      </div>
-    );
-  };
-
-
 
   return (
     <div>
