9ee63a2d7d7df7cbf7be85c41000ecab9d602102
arttujo <arttujo@gmail.com>
2021-04-05 03:53:03 +0300

    script merge

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 while getopts m:b: flag
 do
     case "${flag}" in 
@@ -7,16 +7,14 @@ do
     esac
 done
 
-if [ -z "$message" && -z "$branch"]
-then echo "Please give a message with the flag -m and your branch with -b"
-else
-    echo "Starting Merge to Staging"
-    git add .
-    git commit -m "message"
-    git push
-    git checkout staging 
-    git merge --no-ff staging
-    git push origin staging
-    echo "Done"
-fi
+echo "$message"
+echo "$branch"
 
+git add .
+git commit -m "$message"
+git push origin "$branch"
+git checkout staging
+git merge --no-ff "$branch"
+git push origin staging
+
+echo "Done"
