2cb5dce01590c2f0902eb6dfb09f9ff83ff9bab3
dawcules <ilias.doukas@gmail.com>
2021-04-12 19:26:50 +0300

    sorting by upvotes, category

--- src/components/ideaparts/IdeaList.js
+++ src/components/ideaparts/IdeaList.js
@@ -4,17 +4,15 @@ import GridList from '@material-ui/core/GridList';
 import Container from '@material-ui/core/Container';
 import IdeaCard from '../ideaparts/IdeaCard.js';
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
-
-
+import { Button } from '@material-ui/core';
 
 class IdeaList extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        title: "",
-        body: "",
-        cat: "",
+        list: this.props.data,
       };
+
     }
 
     MyComponent = (props) => {
@@ -34,24 +32,34 @@ class IdeaList extends React.Component {
       return 1;
     };
 
-  
-  
-  
-    myChangeHandler = (event) => {
-      let nam = event.target.name;
-      let val = event.target.value;
-      this.setState({[nam]: val});
-    };
-  
-    render() {
-        let ideas = this.props.data;
+    dynamicSort(property) {
+      var sortOrder = 1;
+      if(property[0] === "-") {
+          sortOrder = -1;
+          property = property.substr(1);
+      }
+      return function (a,b) {
+          var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+          return result * sortOrder;
+      }
+  }
 
+    //TODO minus operator toggle
+    sorter = () => {this.setState( state => ({
+      list: state.list.sort(this.dynamicSort("-category"))
+    }));
+    }
+
+    render() {
+
+      console.log(this.state.list);     
         /* TODO: Sort by Date, upvotes (desc, asc) */
       
         return (
             <Container maxWidth="lg">
+           <Button onClick={() => {this.sorter("categories")}}>SORT</Button>
             <GridList cols={this.MyComponent()} cellHeight="320">
-                {ideas.map((tile) => (
+                {this.state.list.map((tile) => (
     <GridListTile key={tile.img} cols={tile.cols || 1} cellHeight="auto">
       <IdeaCard key={tile} data={tile}></IdeaCard>
     </GridListTile>
